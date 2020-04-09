ade895088c69a3cd12d01f085a2fe13f247f0361
英政 <shiyingzheng@outlook.com>
2020-04-10 06:12:39 +0800

    Update RSA encrypt method2 未完成

--- src/main/java/encrypt/RSAEncrypt.java
+++ src/main/java/encrypt/RSAEncrypt.java
@@ -47,14 +47,16 @@ public class RSAEncrypt {
      * @throws Exception
      *             加密过程中的异常信息
      */
-    public static String encrypt( String str, String publicKey ) throws Exception{
+    public static String decrypt( String str, String publicKey ) throws Exception{
+        //64位解码加密后的字符串
+        byte[] inputByte = Base64.decodeBase64(str.getBytes("UTF-8"));
         //base64编码的公钥
         byte[] decoded = Base64.decodeBase64(publicKey);
         RSAPublicKey pubKey = (RSAPublicKey) KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(decoded));
         //RSA加密
         Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.ENCRYPT_MODE, pubKey);
-        String outStr = Base64.encodeBase64String(cipher.doFinal(str.getBytes("UTF-8")));
+        cipher.init(Cipher.DECRYPT_MODE, pubKey);
+        String outStr = new String(cipher.doFinal(inputByte));
         return outStr;
     }
 
@@ -69,16 +71,14 @@ public class RSAEncrypt {
      * @throws Exception
      *             解密过程中的异常信息
      */
-    public static String decrypt(String str, String privateKey) throws Exception{
-        //64位解码加密后的字符串
-        byte[] inputByte = Base64.decodeBase64(str.getBytes("UTF-8"));
+    public static String encrypt(String str, String privateKey) throws Exception{
         //base64编码的私钥
         byte[] decoded = Base64.decodeBase64(privateKey);
         RSAPrivateKey priKey = (RSAPrivateKey) KeyFactory.getInstance("RSA").generatePrivate(new PKCS8EncodedKeySpec(decoded));
         //RSA解密
         Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.DECRYPT_MODE, priKey);
-        String outStr = new String(cipher.doFinal(inputByte));
+        cipher.init(Cipher.ENCRYPT_MODE, priKey);
+        String outStr =Base64.encodeBase64String(cipher.doFinal(str.getBytes("UTF-8")));
         return outStr;
     }
 
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -7,12 +7,12 @@ public class Main extends RSAEncrypt {
         //生成公钥和私钥
         genKeyPair();
         //加密字符串
-        String message = "加密练习";
-        System.out.println("随机生成的公钥为:" + keyMap.get(0));
-        System.out.println("随机生成的私钥为:" + keyMap.get(1));
-        String messageEn = encrypt(message,keyMap.get(0));
+        String message = "加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习加密练习";
+        System.out.println("随机生成的公钥为:" + keyMap.get(1));
+        System.out.println("随机生成的私钥为:" + keyMap.get(0));
+        String messageEn = encrypt(message,keyMap.get(1));
         System.out.println(message + "\t加密后的字符串为:" + messageEn);
-        String messageDe = decrypt(messageEn,keyMap.get(1));
+        String messageDe = decrypt(messageEn,keyMap.get(0));
         System.out.println("还原后的字符串为:" + messageDe);
     }
 }
