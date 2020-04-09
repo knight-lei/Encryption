fb8132d6d1aa803233044cdc5445acbfd23b79e0
英政 <shiyingzheng@outlook.com>
2020-04-10 07:42:10 +0800

    Update RSA encrypt method to string longer than 128 byte

--- src/main/java/encrypt/RSAEncrypt.java
+++ src/main/java/encrypt/RSAEncrypt.java
@@ -1,7 +1,9 @@
 package encrypt;
 
 import org.apache.commons.codec.binary.Base64;
+
 import javax.crypto.Cipher;
+import java.io.ByteArrayOutputStream;
 import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -11,20 +13,23 @@ import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class RSAEncrypt {
     protected static Map<Integer, String> keyMap = new HashMap<Integer, String>();  //用于封装随机产生的公钥与私钥
+
     /**
      * 随机生成密钥对
+     *
      * @throws NoSuchAlgorithmException
      */
     public static void genKeyPair() throws NoSuchAlgorithmException {
         // KeyPairGenerator类用于生成公钥和私钥对，基于RSA算法生成对象
         KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
         // 初始化密钥对生成器，密钥大小为96-1024位
-        keyPairGen.initialize(1024,new SecureRandom());
+        keyPairGen.initialize(1024, new SecureRandom());
         // 生成一个密钥对，保存在keyPair中
         KeyPair keyPair = keyPairGen.generateKeyPair();
         RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();   // 得到私钥
@@ -33,21 +38,21 @@ public class RSAEncrypt {
         // 得到私钥字符串
         String privateKeyString = new String(Base64.encodeBase64((privateKey.getEncoded())));
         // 将公钥和私钥保存到Map
-        keyMap.put(0,publicKeyString);  //0表示公钥
-        keyMap.put(1,privateKeyString);  //1表示私钥
+        keyMap.put(0, publicKeyString);  //0表示公钥
+        keyMap.put(1, privateKeyString);  //1表示私钥
     }
+
     /**
      * RSA公钥加密
      *
-     * @param str
-     *            加密字符串
-     * @param publicKey
-     *            公钥
+     * @param str       加密字符串
+     * @param publicKey 公钥
      * @return 密文
-     * @throws Exception
-     *             加密过程中的异常信息
+     * @throws Exception 加密过程中的异常信息
      */
-    public static String decrypt( String str, String publicKey ) throws Exception{
+    public static String decrypt(String str, String publicKey) throws Exception {
+        String outStr = "";
+        try {
         //64位解码加密后的字符串
         byte[] inputByte = Base64.decodeBase64(str.getBytes("UTF-8"));
         //base64编码的公钥
@@ -56,32 +61,82 @@ public class RSAEncrypt {
         //RSA加密
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.DECRYPT_MODE, pubKey);
-        String outStr = new String(cipher.doFinal(inputByte));
+            byte[] inputArray = str.getBytes();
+            int inputLength = inputArray.length;
+            System.out.println("解密字节数：" + inputLength);
+            // 最大加密字节数，超出最大字节数需要分组加密
+            int MAX_DECRYPT_BLOCK = 128;
+            // 标识
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            int offSet = 0;
+            int i = 0;
+            byte[] cache = {};
+            while (inputLength - offSet > 0) {
+                if (inputLength - offSet > MAX_DECRYPT_BLOCK) {
+                    cache = cipher.doFinal(inputArray, offSet, MAX_DECRYPT_BLOCK);
+                } else {
+                    cache = cipher.doFinal(inputArray, offSet, inputLength - offSet);
+                }
+                //            out.write(cache, 0, cache.length);
+                out.write(cache);
+                i++;
+                offSet = i * MAX_DECRYPT_BLOCK;
+            }
+            outStr = new String(out.toByteArray());
+            out.close();
+        } catch (Exception e) {
+            System.out.println("rsaDecrypt error:" + e.getMessage());
+        }
+
         return outStr;
     }
 
     /**
      * RSA私钥解密
      *
-     * @param str
-     *            加密字符串
-     * @param privateKey
-     *            私钥
+     * @param str        加密字符串
+     * @param privateKey 私钥
      * @return 铭文
-     * @throws Exception
-     *             解密过程中的异常信息
+     * @throws Exception 解密过程中的异常信息
      */
-    public static String encrypt(String str, String privateKey) throws Exception{
-        //base64编码的私钥
-        byte[] decoded = Base64.decodeBase64(privateKey);
-        RSAPrivateKey priKey = (RSAPrivateKey) KeyFactory.getInstance("RSA").generatePrivate(new PKCS8EncodedKeySpec(decoded));
-        //RSA解密
-        Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.ENCRYPT_MODE, priKey);
-        String outStr =Base64.encodeBase64String(cipher.doFinal(str.getBytes("UTF-8")));
-        return outStr;
-    }
+    public static String encrypt(String str, String privateKey) throws Exception {
+        String outStr = "";
+        try {
+            //base64编码的私钥
+            byte[] decoded = Base64.decodeBase64(privateKey);
+            RSAPrivateKey priKey = (RSAPrivateKey) KeyFactory.getInstance("RSA").generatePrivate(new PKCS8EncodedKeySpec(decoded));
+            //RSA解密
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.ENCRYPT_MODE, priKey);
+            byte[] inputArray = str.getBytes();
+            int inputLength = inputArray.length;
+            System.out.println("加密字节数：" + inputLength);
+            // 最大加密字节数，超出最大字节数需要分组加密
+            int MAX_ENCRYPT_BLOCK = 117;
+            // 标识
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            int offSet = 0;
+            int i = 0;
+            byte[] resultBytes = {};
+            byte[] cache = {};
+            while (inputLength - offSet > 0) {
+                if (inputLength - offSet > MAX_ENCRYPT_BLOCK) {
+                    cache = cipher.doFinal(inputArray, offSet, MAX_ENCRYPT_BLOCK);
+                } else {
+                    cache = cipher.doFinal(inputArray, offSet, inputLength - offSet);
+                }
+                out.write(cache, 0, cache.length);
+                i++;
+                offSet = i * MAX_ENCRYPT_BLOCK;
+            }
+            outStr = Base64.encodeBase64String(out.toByteArray());
+            out.close();
+        } catch (Exception e) {
+            System.out.println("rsaEncrypt error:" + e.getMessage());
+        }
 
+    return outStr;
+    }
 }
 
 
